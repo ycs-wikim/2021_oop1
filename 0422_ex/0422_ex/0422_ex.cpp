a20ea3e73a3cceaea152727236d2f5a5a4752241
wikim <unangel@yuhan.ac.kr>
2021-04-22 15:56:42 +0900

    add code

--- 0422_ex/0422_ex/0422_ex.cpp
+++ 0422_ex/0422_ex/0422_ex.cpp
@@ -3,8 +3,78 @@
 
 #include <iostream>
 
+void fun(int &b)
+{
+	b = 35;
+}
+
+void function()
+{
+	int a;
+	int &ra = a;
+	// int *p = &a;
+
+	//ra = 89;
+	fun(a);
+	// *p = 89;
+	printf("%d\n", a);
+	/*
+	int i = 0;
+	void *ptr = &i;
+	char *p = (char*)ptr;
+	/*
+	int i = 0;
+	int a[3] = { 1, 2, 3 };
+	int *p = a;
+	char c;
+	p = &c;
+	void *v;
+	v = a;
+	v = &i;
+	v = &c;
+	
+
+	for (i = 0; i < 3; i++)
+		printf("%d\n", a[i]);
+	printf("\n");
+	for (i = 0; i < 3; i++)
+		printf("%d\n", *( a + i ));
+	printf("\n");
+	for (i = 0; i < 3; i++)
+		printf("%d\n", *( p + i ) );
+
+	printf("\n");
+	for (i = 0; i < 3; i++)
+		printf("%d\n", p[i]);
+	*/
+}
+
 int main()
 {
+	function();
+	/*
+	int a = 0;
+	char *s = (char*)&a;
+	s++; ++s;		/// s = s + 1; -> + 1 ==> 1 * sizeof( TYPE );
+	s--; --s;		/// s = s - 1; 
+
+	*(s + 0) = 'C';
+	*(s + 1) = '+';		/// s + 1 ==> s + 1 * sizeof( char )
+	*(s + 2) = '+';		/// s + 2 ==> s + ( 2 * sizeof( char ) )
+	*(s + 3) = '\0';	
+	
+	/// int *s; s = s + 1 == > s + 1 * sizeof(int)
+
+	printf("%s\n", (char*)&a);
+	printf("%c%c%c%c\n", *s, *(s + 1), *(s + 2), *(s + 3));
+	printf("%c%c%c%c\n", *((char*)&a), *(((char*)&a) + 1), *(((char*)&a) + 2), *(((char*)&a) + 3));
+
+	/*
+	char c = 65;
+	int *p = (int*)&c;		/// 명시적 형변환
+	int *pp = &c;			/// 오류의 이유: 메모리 블록 처리 문제
+
+	/*
 	int a = 9;
 	int *p = &a;
 	int **pp = &p;
@@ -25,4 +95,5 @@ int main()
 	printf("ppp\tvalue[ %p ] addr[ %p ] *[ %p ] **[ %p ] ***[ %d ] a[ %d ]\n\n", ppp, &ppp, *ppp, **ppp, ***ppp, a);
 
     std::cout << "Hello World!\n";
+	*/
 }
